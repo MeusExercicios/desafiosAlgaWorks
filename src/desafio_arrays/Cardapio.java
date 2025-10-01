fb01ae6e809c9f80acc67cc48cada0ae7f67a8d1
Vitor <vitorcostaofficial@gmail.com>
2025-09-30 22:34:17 -0300

    feat: desafio array

--- src/desafio_arrays/Cardapio.java
+++ src/desafio_arrays/Cardapio.java
@@ -7,18 +7,33 @@ public class Cardapio {
     ItemCardapio[] itens = new ItemCardapio[0];
 
     void adicionarItem(ItemCardapio item) {
-        this.itens = Arrays.copyOf(this.itens,this.itens.length+1);
+        this.itens = Arrays.copyOf(this.itens, this.itens.length + 1);
         itens[itens.length - 1] = item;
     }
 
     void removerItem(int indice) {
-        int [] novosItens = new int [itens.length-1];
         int indiceRemover = indice - 1;
-        System.arraycopy(itens,0,novosItens,0,indiceRemover);
+
+        ItemCardapio[] novosItens = new ItemCardapio[this.itens.length - 1];
+
+        System.arraycopy(this.itens, 0, novosItens, 0, indiceRemover);
+        System.arraycopy(this.itens, indiceRemover + 1, novosItens, indiceRemover, this.itens.length - indiceRemover - 1);
+
+        this.itens = novosItens;
     }
 
     void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
 
+        boolean encontrou = false;
+
+        for (ItemCardapio item : this.itens) {
+
+            if (item.preco >= precoMinimo && item.preco <= precoMaximo) {
+                System.out.printf("- %s (R$ %.2f)%n", item.descricao, item.preco);
+                encontrou = true;
+            }
+
+        }
     }
 
 }
