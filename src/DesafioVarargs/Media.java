964120782b36975cb7868ac7984e562bc0797e28
João Vitor Costa Rolim <121437787+j-vitordev@users.noreply.github.com>
2025-10-02 13:33:04 -0300

    fix: ajuste do desafio

--- src/DesafioVarargs/Media.java
+++ src/DesafioVarargs/Media.java
@@ -2,11 +2,13 @@ package DesafioVarargs;
 
 public class Media {
 
-    static double calcularMedia(int quantidade, double... notas) {
-        double soma = 0.0;
-        for (double nota : notas) {
-            soma += nota;
-        }
-        return soma / quantidade;
+    static double calcularMedia(double... notas) {
+        if (notas.length >= 2) {
+            double soma = 0.0;
+            for (double nota : notas) {
+                soma += nota;
+            }
+            return soma / notas.length;
+        } else return 0;
     }
 }
